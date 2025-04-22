7c1591665b5c3459e01ac3df71fc1412041e388d
Nyein Chan Aung <nyeinchanaung.mr@gmail.com>
2025-04-22 18:02:50 +0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -10,7 +10,7 @@
 
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from functools import wraps
-from helpers import recommender as recommender
+from app.helpers import recommender as recommender
 import pandas as pd
 
 app = Flask(__name__)
