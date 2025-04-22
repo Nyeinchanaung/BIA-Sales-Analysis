cf3c26dfb480b351d6c6a24a8cc5cf5498070107
Phone Myint Naing <francisphone1998@gmail.com>
2025-04-22 21:49:30 +0700

    add feature importance in customer segmentation and data analysis

--- Customer Segmentation/customer_segmentation.py
+++ Customer Segmentation/customer_segmentation.py
@@ -14,4 +14,6 @@ def predict_cluster(recency_days, frequnecy, monetary):
     new_data = [[recency_days, frequnecy, monetary]]
     scaled_data = scaler.transform(new_data)
     predicted_cluster = kmeans.predict(scaled_data)
-    return cluster_definition[predicted_cluster[0]]
+    cluster_center = kmeans.cluster_centers_[predicted_cluster]
+    importance = scaled_data[0] - cluster_center
+    return cluster_definition[predicted_cluster[0]], importance[0]
